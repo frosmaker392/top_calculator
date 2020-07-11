4b5dc533b2a39fe2ebaadf43db374157c78fa034
zhafir <zzzhafir75@gmail.com>
2020-07-11 18:32:26 +0800

    Add the 4 operation functions and the operate function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Test!");
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operations = [add, subtract, multiply, divide];
+
+//Choose an operation in operations array at index, then return
+//the result of said operation with inputs a and b
+function operate(a, b, index){
+    if(index > 3) return null;
+
+    const s = operations[index](a, b);
+    console.log(s);
+}
