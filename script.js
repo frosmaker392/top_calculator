390a066295d8483cd2a6efa18f5f7b8d6673fc07
zhafir <zzzhafir75@gmail.com>
2020-07-12 20:59:53 +0800

    Add listeners to number buttons
    Add addDigit function. Add prototype triggerDP function.

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ function operate(a, b, index){
     const s = operations[index](a, b);
     console.log(s);
 }
+
+(function(){
+    const displayNum = document.querySelector('#display-num');
+    const calcBtns = document.querySelectorAll(".calc-btn");
+
+    displayNum.textContent = 0;
+    let displayValue = 0;
+    //True if decimal point is used
+    let dp = false;
+
+    //Adds a digit at the right of the displayNum value (num * 10 + digit)
+    //Doesn't do anything if displayValue is more than 10 digits long
+    function addDigit(digit){
+        if(digit > 9) throw new Error('digit cannot be greater than 10!');
+        if(displayValue > 999999999) return;
+    
+        displayValue = displayValue * 10 + digit;
+        displayNum.textContent = displayValue;
+    }
+
+    //Adds decimal point, toggles dp to true
+    //Does nothing if dp is already true
+    function triggerDP(){
+        if(dp) return;
+        //TODO
+        dp = true;
+    }
+
+    calcBtns.forEach((btn) => {
+        if(btn.classList.contains('num')){
+            btn.addEventListener('click', (e) => {
+                addDigit(+e.target.id);
+            });
+        }
+        else if(btn.classList.contains('op')){
+            //TODO
+        }
+        else if(btn.classList.contains('mod')){
+            switch(btn.id){
+                case 'point':
+                    triggerDP();
+                    break;
+            }
+        }
+    });
+})();
+
