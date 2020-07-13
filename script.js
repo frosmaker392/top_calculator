ecf4c41bef8d84188270684c2e09bfd125f65bdb
zhafir <zzzhafir75@gmail.com>
2020-07-13 15:50:07 +0800

    Add backspace functionality, implement triggerDP
    Redo addDigit function to push char on an array (which will be parsed as a
    float later). Add several checks to avoid left trailing zeroes and for digitCount
    correction. Fix case statement before that runs the function instead of adding
    the event listener.

--- script.js
+++ script.js
@@ -18,44 +18,96 @@ function operate(a, b, index){
     const displayNum = document.querySelector('#display-num');
     const calcBtns = document.querySelectorAll(".calc-btn");
 
-    displayNum.textContent = 0;
-    let displayValue = 0;
+    let displayCharArr = [0];
+    let digitCount = 0;
+
     //True if decimal point is used
     let dp = false;
+    //True if leftmost digit is a zero
+    let rejectZero = true;
+
+    displayNum.textContent = 0;
+
+    function updateDisplay(){
+        displayNum.textContent = displayCharArr.join('');
+    }
 
     //Adds a digit at the right of the displayNum value (num * 10 + digit)
-    //Doesn't do anything if displayValue is more than 10 digits long
-    function addDigit(digit){
-        if(digit > 9) throw new Error('digit cannot be greater than 10!');
-        if(displayValue > 999999999) return;
-    
-        displayValue = displayValue * 10 + digit;
-        displayNum.textContent = displayValue;
+    //Doesn't do anything if number of digits equal 10
+    function addDigit(digitChar){
+        if(digitCount === 10) return;
+
+        if(!rejectZero || digitChar !== '0'){
+            if(digitCount !== 0){
+                displayCharArr.push(digitChar);    
+            }
+            else{
+                displayCharArr[0] = digitChar;
+            }
+            updateDisplay();
+            digitCount++;
+            rejectZero = false;
+        }
     }
 
     //Adds decimal point, toggles dp to true
     //Does nothing if dp is already true
     function triggerDP(){
         if(dp) return;
-        //TODO
+
+        if(digitCount === 0){
+            rejectZero = false;
+            digitCount++;
+        }
+        displayCharArr.push('.');
+        updateDisplay();
         dp = true;
+    }
+
+    //Removes last digit/dp in the display char array
+    function backspace(){
+        if(digitCount === 0) return;
+
+        const removedElement = displayCharArr.pop();
+        if(removedElement === '.'){
+            if(digitCount === 1 && displayCharArr[0] === '0'){
+                digitCount--;
+            }
+            else{
+                dp = false;
+            }
+        }
+        else digitCount--;
+
+        if(digitCount === 0){
+            displayNum.textContent = 0;
+            rejectZero = true;
+        }
+        else
+            updateDisplay();
     }
 
     calcBtns.forEach((btn) => {
         if(btn.classList.contains('num')){
             btn.addEventListener('click', (e) => {
-                addDigit(+e.target.id);
+                addDigit(e.target.id);
             });
         }
         else if(btn.classList.contains('op')){
             //TODO
         }
         else if(btn.classList.contains('mod')){
+            let onClick;
             switch(btn.id){
                 case 'point':
-                    triggerDP();
+                    onClick = triggerDP;
+                    break;
+                case 'back':
+                    onClick = backspace;
                     break;
             }
+
+            btn.addEventListener('click', onClick);
         }
     });
 })();
